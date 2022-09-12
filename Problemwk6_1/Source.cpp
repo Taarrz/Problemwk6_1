51c45fe5ce314020aae79856d350231ea570541d
Taarrz <65010395@kmitl.ac.th>
2022-09-12 17:02:37 +0700

    Add project files.

---
+++ Problemwk6_1/Source.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int Combination(int x, int y);
+int Factorial(int x);
+int main() {
+	int n;
+	printf("N = ");
+	scanf_s("%d", &n, sizeof(n));
+	int handshake = Combination(n, 2);
+	printf("Handshake = %d", handshake);
+	return 0;
+}
+
+int Factorial(int n) {
+	if (n == 1) {
+		return 1;
+	}
+	else {
+		return n * Factorial(n - 1);
+	}
+	
+}
+
+int Combination(int n, int r) {
+	int result = Factorial(n) / (Factorial(r) * Factorial(n - r));
+	return result;
+}
